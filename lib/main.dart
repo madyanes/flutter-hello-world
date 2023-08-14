425e11c00ffaff6620a1454803ec3ee9a027dfb0
madyanes <madyanes.dev@gmail.com>
2023-08-14 16:35:36 +0700

    create BiggerText stateful widget widget jenis stateful yang pertama kali saya buat

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const Scaffold(
-        body: Center(
-          child: Heading(
-            text: 'Hello, world!',
-          ),
-        ),
+        body: Center(child: BiggerText(text: 'Hello world!')),
       ),
     );
   }
@@ -36,6 +32,37 @@ class Heading extends StatelessWidget {
         fontSize: 24.0,
         fontWeight: FontWeight.bold,
       ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: const Text('Perbesar'),
+          onPressed: () {
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
     );
   }
 }
